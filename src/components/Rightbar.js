463b9e3ddbd1337f676030dd8f3368e647da7df9
douknowccy <twkccy1990@gmail.com>
2020-11-16 12:51:08 +0800

    first

---
+++ src/components/Rightbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Rightbar() {
+  return <div>Right bar</div>;
+}
+
+export default Rightbar;
---
+++ src/pages/DashBoard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DashBoard() {
+  return <div>DashBoad Page</div>;
+}
+
+export default DashBoard;
